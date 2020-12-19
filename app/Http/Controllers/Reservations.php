6b03b5f1278a34f5f26123a8f8d30d5046756c1d
Mrgurmanas <Paulius543210@gmail.com>
2020-12-19 15:05:12 +0200

    added reservation tables

--- app/Http/Controllers/Reservations.php
+++ app/Http/Controllers/Reservations.php
@@ -8,7 +8,15 @@ class Reservations extends Controller
 {
     //
     function search(Request $req){
-        print_r($req -> input());
+        //print_r($req -> input());
+        $search_town = $_POST['town'];
+        $search_from = $_POST['datefrom'];
+        $search_to = $_POST['dateto'];
+        print_r($search_town);
+        print_r($search_from);
+        print_r($search_to);
+
+
         //redirect()->back();
     }
 }
