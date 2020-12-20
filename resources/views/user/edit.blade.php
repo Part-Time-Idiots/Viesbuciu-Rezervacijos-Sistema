87cbefbf7a2ec53b89429aa63898600afaea33bf
aurimasvarnelis <aurimasvarnelis@gmail.com>
2020-12-21 01:01:14 +0200

    Added more user settings

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -34,6 +34,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Pavardė') }}</label>
+    
+                                <div class="col-md-6">
+                                    <input id="surname" value="{{ $user['surname'] }}" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" autocomplete="surname" autofocus>
+    
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('El. pašto adresas') }}</label>
 
                             <div class="col-md-6">
@@ -46,6 +60,34 @@
                                 @enderror
                             </div>
                         </div>
+
+                        <div class="form-group row">
+                            <label for="birthday" class="col-md-4 col-form-label text-md-right">{{ __('Gimimo data') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="birthday" value="{{ $user['birthday'] }}" type="date" class="form-control @error('birthday') is-invalid @enderror" name="birthday" value="{{ old('birthday') }}" autocomplete="birthday">
+
+                                @error('birthday')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="personal_code" class="col-md-4 col-form-label text-md-right">{{ __('Asmens kodas') }}</label>
+    
+                                <div class="col-md-6">
+                                    <input id="personal_code" value="{{ $user['personal_code'] }}" type="text" class="form-control @error('personal_code') is-invalid @enderror" name="personal_code" value="{{ old('personal_code') }}" autocomplete="personal_code" autofocus>
+    
+                                    @error('personal_code')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
